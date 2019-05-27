a17675fef56a41294c08134fa97df78f905fb9fe
omody <omarmody@gmail.com>
2019-05-27 04:40:45 -0700

    nightly update app

--- home/pi/scripts/nightly_reboot.sh
+++ home/pi/scripts/nightly_reboot.sh
@@ -5,6 +5,18 @@ then
 exit
 fi
 
+cd /home/pi/blissflixx
+/usr/bin/git fetch --all
+/usr/bin/git reset --hard origin/master
+/usr/bin/git pull
+cd /home/pi/blissflixx/lib/youtube-dl
+/usr/bin/make lazy-extractors
+/usr/bin/make youtube-dl
+chmod +x /home/pi/blissflixx/*.sh
+chmod +x /home/pi/blissflixx/bin/*
+chmod +x /home/pi/blissflixx/blissflixx.py
+chmod +x /home/pi/blissflixx/*
+
 #sudo pip install --upgrade youtube-dl
 
 #sudo reboot
